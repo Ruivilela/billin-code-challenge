6853f7b3cf1fe9386ad97a55bc7427407ff7b76d
Rui Miguel Marques Vilela <ruimmvilela@gmail.com>
2017-07-23 21:35:28 +0200

    styles added to cards component

--- src/components/cards/component.js
+++ src/components/cards/component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import request from './../../request';
 import { ARTICLES_QUERY } from './../../queries';
+import style from './style';
 
 export default class Cards extends Component {
   // lifecycle
@@ -15,11 +16,11 @@ export default class Cards extends Component {
   render() {
     let articles = this.props.articles.map((articles) => {
       return(
-        <div key={articles.id}>
+        <div style={style.box} key={articles.id}>
           <h2 style={style.author}>
             {articles.author}
           </h2>
-          <h6>
+          <h6 style={style.title}>
             Title: {articles.title}
           </h6>
           <div>
@@ -29,16 +30,10 @@ export default class Cards extends Component {
       )
     })
     return (
-      <div className="App">
+      <div className="App" style={style.component}>
         {articles}
       </div>
     );
-  }
-}
-
-const style = {
-  author:{
-    margin:0
   }
 }
 // <h2>Billin code challenge</h2>
---
+++ src/components/cards/style.js
@@ -0,0 +1,24 @@
+const style = {
+  component: {
+    marginLeft:'5%',
+    marginTop:'5%'
+  },
+  box: {
+    marginBottom:'2%',
+    marginLeft:'5%',
+    width:'38%',
+    height:'100%',
+    padding:'1%',
+    border:'2px solid black',
+    float:'left'
+  },
+  author: {
+    margin:0
+  },
+  title: {
+    margin:0,
+    marginBottom:'5%'
+  }
+}
+
+export default style
