feda8804d419ac306363e70478f55247de1ddbc1
Rui Miguel Marques Vilela <ruimmvilela@gmail.com>
2017-08-06 01:48:58 +0200

    create, update and delete articles added to graphql schema

--- server/schema.js
+++ server/schema.js
@@ -6,7 +6,7 @@ import {
   GraphQLString,
   GraphQLList,
   GraphQLSchema,
-  GraphQLInt
+  GraphQLInt,
 } from 'graphql';
 import db from './db';
 
@@ -62,8 +62,91 @@ const Query = new GraphQLObjectType({
   }),
 });
 
+const Mutation = new GraphQLObjectType({
+  name:'mutations',
+  description:'these are the things we can change',
+  fields: () => ({
+    createArticle: {
+      type: new GraphQLList(articleType),
+      description: 'creates article',
+      args: {
+        author: {
+          type: GraphQLString,
+        },
+        content: {
+          type: GraphQLString,
+        },
+        excerpt: {
+          type: GraphQLString,
+        },
+        published: {
+          type: GraphQLBoolean,
+        },
+        tags: {
+          type: new GraphQLList(GraphQLString),
+        },
+        title: {
+          type: GraphQLString,
+        }
+      },
+      resolve: (obj, args, context) => {
+        return db.Article.create(args)
+          .then((article) => {
+            article
+          })
+      }
+    },
+    deleteArticle: {
+      type: new GraphQLList(articleType),
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: (obj, args, context) => {
+        return db.Article.remove({_id:new ObjectId(args.id)})
+          .then((article) => {
+            article
+          })
+      }
+    },
+    updateArticle:{
+      type: new GraphQLList(articleType),
+      args: {
+        id: { type: GraphQLString },
+        author: {
+          type: GraphQLString,
+        },
+        content: {
+          type: GraphQLString,
+        },
+        excerpt: {
+          type: GraphQLString,
+        },
+        published: {
+          type: GraphQLBoolean,
+        },
+        tags: {
+          type: new GraphQLList(GraphQLString),
+        },
+        title: {
+          type: GraphQLString,
+        }
+      },
+      resolve: (obj, args, context) => {
+        return db.Article.find(
+          {_id:new ObjectId(args.id)}
+        ).update(args)
+          .then((article) => {
+            article
+          })
+      }
+    }
+  })
+})
+
+
 const Schema = new GraphQLSchema({
   query: Query,
+  mutation: Mutation
 });
 
 export default Schema;
